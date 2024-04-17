94598b632d901787bb59fe0adfe97d925a01ca3b
a2sy <arifardiansyah@students.usu.ac.id>
2024-04-17 13:57:55 +0700

    add .config

---
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,30 @@
+-- EXAMPLE 
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = { "html", "cssls", "rust_analyzer" }
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
+end
+
+-- typescript
+-- lspconfig.tsserver.setup {
+--   on_attach = on_attach,
+--   on_init = on_init,
+--   capabilities = capabilities,
+-- }
+
+-- rust-analyzer
+-- lspconfig.rust-analyzer.setup {
+--   on_attach = on_attach,
+--   on_init = on_init,
+--   capabilities = capabilities,
+-- }
